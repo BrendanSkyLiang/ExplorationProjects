2cf04baf36a797cf31848ac4c1d9781825350a1b
BrendanSkyLiang <87059304+BrendanSkyLiang@users.noreply.github.com>
2021-07-27 10:50:17 +0800

    Update sodukuSolver.py

--- sodukuSolver.py
+++ sodukuSolver.py
@@ -5,16 +5,13 @@ Created on Tue Jul 27 09:00:00 2021
 @author: brendanlia
 """
 
-
 def printBoard(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
-
+            print("- - - - - - - - - - - - ")
         for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
-
             if j == 8:
                 print(board[i][j])
             else:
@@ -25,7 +22,6 @@ def findEmpty(board):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)  # row, col
-
     return None
 
 def valid(board, num , row, col):
@@ -37,7 +33,6 @@ def valid(board, num , row, col):
             return False
     boxX = col // 3
     boxY = row // 3
-    
     for i in range(boxX*3, boxY*3+3):
         for j in range(boxY*3, boxX*3+3):
             if board[j][i] == num and (j,i) != (row, col):
@@ -58,9 +53,8 @@ def solve(board):
             board[row][col] = 0
     return False
 
-
 board = [
-    [7,8,0,4,0,0,1,2,0],
+    [7,5,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
     [0,0,7,0,4,0,2,6,0],
@@ -71,10 +65,10 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-
 printBoard(board)
 solve(board)
 print('---------------------------------------------')
+print('Solution')
 printBoard(board)
 
 
